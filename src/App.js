b16b87d8bada9d7bc048f90588c7ab0dbb895d77
anastasiia <kholina.a@gmail.com>
2023-04-11 15:40:19 +0200

    detachment of navigation into separate component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import React  from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './Components/Home/Home';
+
+import Wishlist from './Components/Home/WishlistComponent/Wishlist';
+import Contact from './Components/Contact/Contact';
+import About from './Components/About/About';
 import './App.css';
 import Navigation from './Components/Navigation';
 
@@ -5,7 +16,15 @@ import Navigation from './Components/Navigation';
 function App() {
 
   return (
-    <Navigation />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path='/' element=<Home /> />
+        <Route path='/about' element=<About /> />
+        <Route path='/contact' element=<Contact />/>
+        <Route path='/wishlist' element=<Wishlist />/>
+      </Routes>
+    </Router>
   );
 }
 
